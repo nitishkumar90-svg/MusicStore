765cab6c1cc35144a470ddc66bea8e1aa6158e69
Nitish Kumar <70220240+nitishkumar90-svg@users.noreply.github.com>
2020-12-17 00:30:25 +0530

    new file for musicList in array

---
+++ source/js/music-list.js
@@ -0,0 +1,104 @@
+//drum beat list
+let drumBeatsList = [
+    {
+        "id": "crash",
+        "name": "crash.wav",
+        "url": "/MusicStore/source/resources/audio/drum/crash.wav"
+    },
+    {
+        "id": "kick",
+        "name": "kick.wav",
+        "url": "/MusicStore/source/resources/audio/drum/kick.wav"
+    },
+    {
+        "id": "snare",
+        "name": "snare.wav",
+        "url": "/MusicStore/source/resources/audio/drum/snare.wav"
+    },
+    {
+        "id": "tom",
+        "name": "tom.wav",
+        "url": "/MusicStore/source/resources/audio/drum/tom1.wav"
+    },
+    {
+        "id": "highhat",
+        "name": "highhat.wav",
+        "url": "/MusicStore/source/resources/audio/drum/highhat-open.wav"
+    }
+]
+
+//songs list
+let availableSongsList = [
+    {
+        "id": "betterdays",
+        "name": "Better Days",
+        "url": "/MusicStore/source/resources/audio/music/betterdays.mp3",
+        "imageUrl": "/MusicStore/source/resources/images/weekly1.jpg",
+        "min": 0,
+        "max": 300
+    },
+    {
+        "id": "dubstep",
+        "name": "Dub Step",
+        "url": "/MusicStore/source/resources/audio/music/dubstep.mp3",
+        "imageUrl": "/MusicStore/source/resources/images/weekly2.jpg",
+        "min": 0,
+        "max": 300
+    },
+    {
+        "id": "energy",
+        "name": "Energy",
+        "url": "/MusicStore/source/resources/audio/music/energy.mp3",
+        "imageUrl": "/MusicStore/source/resources/images/weekly3.jpg",
+        "min": 0,
+        "max": 300
+    },
+    {
+        "id": "epic",
+        "name": "Epic",
+        "url": "/MusicStore/source/resources/audio/music/epic.mp3",
+        "imageUrl": "/MusicStore/source/resources/images/weekly4.jpg",
+        "min": 0,
+        "max": 300
+    },
+    {
+        "id": "onceagain",
+        "name": "Once Again",
+        "url": "/MusicStore/source/resources/audio/music/onceagain.mp3",
+        "imageUrl": "/MusicStore/source/resources/images/weekly5.jpg",
+        "min": 0,
+        "max": 300
+    },
+    {
+        "id": "slowmotion",
+        "name": "Slow Motion",
+        "url": "/MusicStore/source/resources/audio/music/slowmotion.mp3",
+        "imageUrl": "/MusicStore/source/resources/images/weekly6.jpg",
+        "min": 0,
+        "max": 300
+    },
+    {
+        "id": "sunny",
+        "name": "Sunny",
+        "url": "/MusicStore/source/resources/audio/music/sunny.mp3",
+        "imageUrl": "/MusicStore/source/resources/images/weekly7.jpg",
+        "min": 0,
+        "max": 300
+    },
+    {
+        "id": "tenderness",
+        "name": "Tenderness",
+        "url": "/MusicStore/source/resources/audio/music/tenderness.mp3",
+        "imageUrl": "/MusicStore/source/resources/images/weekly8.jpg",
+        "min": 0,
+        "max": 300
+    },
+    {
+        "id": "sunny-ext",
+        "name": "Sunny",
+        "url": "/MusicStore/source/resources/audio/music/sunny.mp3",
+        "imageUrl": "/MusicStore/source/resources/images/weekly9.jpg",
+        "min": 0,
+        "max": 300
+    }
+]
